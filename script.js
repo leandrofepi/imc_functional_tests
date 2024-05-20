2184c0daa5f951c2ea62a463eb1efefba3a1232f
Leandro Duarte <lduarte@unifei.edu.br>
2024-05-20 15:36:45 -0300

    whitespaces remove

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function imc() {
     const nome = document.getElementById('nome').value;
     const altura = document.getElementById('altura').value;
     const peso = document.getElementById('peso').value;
-    const resultado = document.getElementById('resultado');
+    const resultado = document.getElementById('resultado');   
 
     if (nome !== '' && altura !== '' && peso !== '') {
 
